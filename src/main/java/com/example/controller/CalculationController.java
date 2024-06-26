21b284d458f9b5b16a7c2c3a517339b6b9084f6c
Naushad101 <naushad.shaikh@bnt-soft.com>
2024-06-27 00:48:38 +0530

    Executor Framework

---
+++ src/main/java/com/example/controller/CalculationController.java
@@ -0,0 +1,25 @@
+package com.example.controller;
+
+import com.example.service.CalculationService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import java.util.Scanner;
+
+@RestController
+public class CalculationController {
+    Scanner sc = new Scanner(System.in);
+    int number = sc.nextInt();
+    static{
+        System.out.println("Enter the number:");
+    }
+    CalculationService service = new CalculationService(number);
+
+
+    @GetMapping("/run")
+    public void getAllrun() {
+         // Example number
+        service.executeTasks();
+    }
+}
---
+++ src/main/java/com/example/service/CalculationService.java
@@ -0,0 +1,142 @@
+package com.example.service;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class CalculationService {
+
+    private int number;
+
+    public CalculationService(int number) {
+        this.number = number;
+    }
+
+    public void executeTasks() {
+        ExecutorService executorService = null;
+
+        try {
+            executorService=Executors.newFixedThreadPool(8);
+        executorService.submit(this::squareTask);
+        executorService.submit(this::cubeTask);
+        executorService.submit(this::factorialTask);
+        executorService.submit(this::armstrongTask);
+        executorService.submit(this::palindromeTask);
+        executorService.submit(this::evenTask);
+        executorService.submit(this::primeTask);
+        executorService.submit(this::reverseTask);
+        } catch (Exception e) {
+            log.warn("Error Occured ",e.getMessage());
+        }
+        finally{
+            executorService.shutdown();
+        }
+    }
+
+    private void squareTask() {
+        int result = square(number);
+        System.out.println("Square of " + number + " is: " + result);
+    }
+
+    private void cubeTask() {
+        int result = cube(number);
+        System.out.println("Cube of " + number + " is: " + result);
+    }
+
+    private void factorialTask() {
+        int result = factorial(number);
+        System.out.println("Factorial of " + number + " is: " + result);
+    }
+
+    private void armstrongTask() {
+        boolean result = isArmstrong(number);
+        System.out.println(number + " is Armstrong: " + result);
+    }
+
+    private void palindromeTask() {
+        boolean result = isPalindrome(number);
+        System.out.println(number + " is palindrome: " + result);
+    }
+
+    private void evenTask() {
+        boolean result = isEven(number);
+        System.out.println(number + " is even: " + result);
+    }
+
+    private void primeTask() {
+        boolean result = isPrime(number);
+        System.out.println(number + " is prime: " + result);
+    }
+
+    private void reverseTask() {
+        int result = reverse(number);
+        System.out.println("Reverse of " + number + " is: " + result);
+    }
+
+    private int square(int number) {
+        return number * number;
+    }
+
+    private int cube(int number) {
+        return number * number * number;
+    }
+
+    private int factorial(int number) {
+        int fact = 1;
+        for (int i = 2; i <= number; i++) {
+            fact *= i;
+        }
+        return fact;
+    }
+
+    private boolean isArmstrong(int number) {
+        int originalNumber = number;
+        int sum = 0;
+        int digits = String.valueOf(number).length();
+        while (number != 0) {
+            int digit = number % 10;
+            sum += Math.pow(digit, digits);
+            number /= 10;
+        }
+        return originalNumber == sum;
+    }
+
+    private boolean isPalindrome(int number) {
+        int originalNumber = number;
+        int reversedNumber = 0;
+        while (number != 0) {
+            int digit = number % 10;
+            reversedNumber = reversedNumber * 10 + digit;
+            number /= 10;
+        }
+        return originalNumber == reversedNumber;
+    }
+
+    private boolean isEven(int number) {
+        return number % 2 == 0;
+    }
+
+    private boolean isPrime(int number) {
+        if (number <= 1) {
+            return false;
+        }
+        for (int i = 2; i <= Math.sqrt(number); i++) {
+            if (number % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private int reverse(int number) {
+        int reversedNumber = 0;
+        while (number != 0) {
+            int digit = number % 10;
+            reversedNumber = reversedNumber * 10 + digit;
+            number /= 10;
+        }
+        return reversedNumber;
+    }
+}
